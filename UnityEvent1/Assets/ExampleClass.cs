8216d6d17b93a2d3a3355785c4fc39e20f3cdfd0
linminquan <leolmq@gmail.com>
2017-03-12 15:32:23 +0800

    add UnityEvent1

---
+++ UnityEvent1/Assets/ExampleClass.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class MyIntEvent : UnityEvent<int>
+{
+}
+
+public class ExampleClass : MonoBehaviour
+{
+	public MyIntEvent m_MyEvent;
+
+	void Start ()
+	{
+		if (m_MyEvent == null)
+			m_MyEvent = new MyIntEvent();
+
+		m_MyEvent.AddListener (Ping);
+	}
+
+	void Update () 
+	{
+		if (Input.anyKeyDown && m_MyEvent != null)
+		{
+			m_MyEvent.Invoke (5);
+		}
+	}
+
+	void Ping (int i)
+	{
+		Debug.Log ("Ping" + i);
+	}
+}
