47b05abec4ea18d47c31dd063386e885fcc35161
SZCGTIGER\linminquan <leolmq@gmail.com>
2017-03-17 15:06:04 +0800

    add BuildDllForUnity-UnityProject

---
+++ BuildDllForUnity/BuildDllForUnity-UnityProject/Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DLLTest;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MyUtilities utils = new MyUtilities ();
+		utils.AddValues (2, 3);
+		print("2 + 3 = " + utils.c);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(MyUtilities.GenerateRandom(0, 100));
+	}
+}
---
+++ BuildDllForUnity/BuildDllForUnity-VisualStudioProject/DLLTest/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace DLLTest
+{
+    public class MyUtilities
+    {
+        public int c;
+
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+    }
+}
